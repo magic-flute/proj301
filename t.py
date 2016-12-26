2d2dc34792088190f495e667868d0aa098470985
magic-flute <mail706843@mailout.e4ward.com>
2016-12-26 03:14:02 +0200

    Create t.py

---
+++ t.py
@@ -0,0 +1,31 @@
+import re
+
+print 'Hello, world!'
+
+
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "iteration {iteration} is {name}".format(iteration=i, name=name)
+
+
+
+parents, babies = (1, 1)
+while babies < 100:
+    print 'This generation has {0} babies'.format(babies)
+    parents, babies = (babies, parents + babies)
+
+
+def greet(name):
+    print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+
+
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print test_string, 'is a valid US local phone number'
+    else:
+        print test_string, 'rejected'
